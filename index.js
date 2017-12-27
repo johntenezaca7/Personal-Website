c5efe1d49127057d6e8392c22f0d52f8a4d8458d
John Tenezaca <john.tenezaca7@gmail.com>
2017-12-27 17:21:32 -0500

    added create react app for front end

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ app.get('/', (req, res) => {
 })
 
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'));
+// if(process.env.NODE_ENV === 'production'){
+//     app.use(express.static('client/build'));
 
-    const path = require('path');
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });    
-}
+//     const path = require('path');
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//     });    
+// }
 
 app.listen(PORT, () => {
     console.log('Server started at port: ' + PORT +' !');
