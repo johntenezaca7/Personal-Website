f58e907037025c5080d04f54d10a598cf30e4c12
John Tenezaca <john.tenezaca7@gmail.com>
2017-12-27 17:08:35 -0500

    inital commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send({hi: "Hello"})
+})
+
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });    
+}
+
+app.listen(PORT, () => {
+    console.log('Server started at port: ' + PORT +' !');
+})
+
+// https://floating-citadel-41183.herokuapp.com/ 
