f8fce7a3568054e1f83a622f700e384bb7834895
John Tenezaca <john.tenezaca7@gmail.com>
2018-01-04 21:29:08 -0500

    added bar graph to display skills/ made it responsive

--- client/src/components/resume.js
+++ client/src/components/resume.js
@@ -9,9 +9,85 @@ const Resume = () => {
                 <div className="res-skills">
                     <h3>Skills</h3>
                     <h4>Programing</h4>
+                    <div className="for-mobile">
                     <h5>Strong : <br/><span id="tab">JavaScript (ES6)</span>, HTML/CSS, Bootstrap, ReactJS, Redux, NodeJS, ExpressJS, Git, MongoDB, MySQL, Mongoose </h5>
                     <h5>Experienced : <br /> <span id="tab"> Mocha/Jest/Chai</span>,  jQuery, AWS, Socket.io, Firebase, Bookshelf, Knex, PostgresSQL </h5>
+                    </div>
+                    <div className="skills">
+                        <ul className="lines">
+                            <li className="line l--0">
+                            <span className="line__label title">
+                                Skill level:
+                            </span>
+                            </li>
+                            <li className="line l--25">
+                            <span className="line__label">
+                                Novice
+                            </span>
+                            </li>
+                            <li className="line l--50">
+                            <span className="line__label">
+                                Intermediate
+                            </span>
+                            </li>
+                            <li className="line l--75">
+                            <span className="line__label">
+                                Strong
+                            </span>
+                            </li>
+                            <li className="line l--100">
+                            <span className="line__label">
+                                Expert
+                            </span>
+                            </li>
+                        </ul>
+                        
+                        <div className="charts">
+                            <div className="chart chart--dev">
+                            <span className="chart__title">Development</span>
+                            <ul className="chart--horiz">
+                                <li className="chart__bar" style={{'width': '85%'}}>
+                                <span className="chart__label">
+                                    HTML/CSS
+                                </span>
+                                </li>
+                                <li className="chart__bar" style={{'width': '70%'}}>
+                                <span className="chart__label">
+                                    JavaScript
+                                </span>
+                                </li>
+                                <li className="chart__bar" style={{'width': '70%'}}>
+                                <span className="chart__label">
+                                    ReactJS + Redux
+                                </span>
+                                </li>
+                                <li className="chart__bar" style={{'width': '65%'}}>
+                                <span className="chart__label">
+                                    NodeJS + ExpressJS
+                                </span>
+                                </li>
+                                <li className="chart__bar" style={{'width': '60%'}}>
+                                <span className="chart__label">
+                                MongoDB + Mongoose
+                                </span>
+                                </li>
+                                <li className="chart__bar" style={{'width': '60%'}}>
+                                <span className="chart__label">
+                                    MySQL + Sequelize
+                                </span>
+                                </li>
+                                <li className="chart__bar" style={{'width': '80%'}}>
+                                <span className="chart__label">
+                                Git + Github
+                                </span>
+                            </li>
+                            </ul>
+                            </div>
+                        </div>
+                        </div>
+                    
                 </div>
+              
                 <div className="res-ex">
                     <h3>Experience</h3>
     
